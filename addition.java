cfb74296c9cad5fb0fb320da12ebd9dd931b5b07
R Prithiviraj <itsprithiv@gmail.com>
2019-09-19 11:11:00 +0530

    to add two numbers

---
+++ addition.java
@@ -0,0 +1,12 @@
+
+class addition {
+    public static void main(String[] args) {
+ 
+ int n1,n2,sum=0;
+        n1 = Integer.parseInt(args[0]);
+ n2 = Integer.parseInt(args[1]);
+ 
+ sum = n1+n2;
+        System.out.println("Sum of 2 nos = "+sum); 
+    }
+}
